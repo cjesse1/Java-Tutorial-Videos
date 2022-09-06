3ce901e1e6dd58ed3eedcb4e7ac865fabd7f20a7
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-09-06 16:01:35 +0100

    .

---
+++ src/newvideo12.java
@@ -0,0 +1,24 @@
+public class newvideo12 {
+    public static void main(String[]args) {
+        int m = 5 / 2;
+        System.out.println(m);
+        double n = 5 / 2;
+        System.out.println(n);
+        double o = (double) 5 / 2;
+        System.out.println(o);
+        int p = 5;
+        p++;
+        System.out.println(p);
+        int q = 5;
+        q--;
+        System.out.println(q);
+        int r = 10;
+        int s = r++;
+        System.out.println(r);
+        System.out.println(s);
+        int t = 20;
+        int u = ++t;
+        System.out.println(t);
+        System.out.println(u);
+    }
+}
