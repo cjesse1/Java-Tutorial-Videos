fb01f68b77052a7b2de1907d6d36c9481bd99f40
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-09-06 16:11:43 +0100

    .

---
+++ src/newvideo13.java
@@ -0,0 +1,7 @@
+public class newvideo13 {
+    public static void main(String[]args){
+        int x = 10;
+        int y = 20;
+        System.out.println(Integer.max(x, y));
+        System.out.println(Integer.compare(x, y));
+}
