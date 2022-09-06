03448322c7427391572f5740c7e593ebd882524d
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-09-07 00:48:53 +0100

    .

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    public String FirstName;
+    public String LastName;
+
+    public String getFullName(){
+        return FirstName + " " + LastName;
+
+    }
+}
---
+++ src/newvideo17.java
@@ -0,0 +1,9 @@
+public class newvideo17 {
+    public static void main(String[]args){
+        User user = new User();
+        user.FirstName = "Caleb";
+        user.LastName = "curry";
+        System.out.println(user.getFullName());
+
+    }
+}
