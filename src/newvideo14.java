84d4e24a4c3fca313f8acfde2833a48688d2abc2
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-09-06 16:20:36 +0100

    .

---
+++ src/newvideo14.java
@@ -0,0 +1,16 @@
+public class newvideo14 {
+    public static void main(String[]args){
+        String x = "hello";
+        System.out.println(x.charAt(1));
+        String y = "hello\n\ncats";
+        System.out.println(y);
+        String z = "hello cats";
+        System.out.println(z);
+        System.out.println(z + " and dogs");
+        z += " and puppies";
+        System.out.println(z);
+        String name = "caleb";
+        System.out.println(String.format("hello %s", name));
+        System.out.println(name.length());
+    }
+}
