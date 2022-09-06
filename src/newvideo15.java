ab384bb9ed45c7538ee536c48301da9afe3b4dcf
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-09-06 16:54:29 +0100

    .

---
+++ src/newvideo15.java
@@ -0,0 +1,10 @@
+public class newvideo15 {
+    public static void main(String[]args){
+        String x = "hello my name is jesse";
+        System.out.println(x.charAt(10));
+        System.out.println(x.charAt(x.length() - 1));
+        System.out.println("I said: " + x);
+        System.out.println(x.contains("is"));
+
+    }
+}
